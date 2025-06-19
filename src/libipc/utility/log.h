32d828ea574e928d1b11b71e286f98375e8e857a
MicroBlock <66859419+std-microblock@users.noreply.github.com>
2025-06-19 23:08:49 +0800

    feat: support running in chromium sandboxed environment

--- src/libipc/utility/log.h
+++ src/libipc/utility/log.h
@@ -2,18 +2,50 @@
 
 #include <cstdio>
 #include <utility>
+#include <iostream>
+#include <string>
+#include <cstdarg>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 namespace ipc {
 namespace detail {
-
 template <typename O>
 void print(O out, char const * str) {
-    std::fprintf(out, "%s", str);
+#ifdef _WIN32
+    HANDLE hConsole = (out == stdout) ? GetStdHandle(STD_OUTPUT_HANDLE) : GetStdHandle(STD_ERROR_HANDLE);
+    DWORD written;
+    WriteConsoleA(hConsole, str, strlen(str), &written, nullptr);
+    OutputDebugStringA(str);
+#else
+    if (out == stdout) {
+        std::cout << str;
+    } else {
+        std::cerr << str;
+    }
+#endif
 }
 
 template <typename O, typename P1, typename... P>
 void print(O out, char const * fmt, P1&& p1, P&&... params) {
-    std::fprintf(out, fmt, std::forward<P1>(p1), std::forward<P>(params)...);
+    constexpr size_t buffer_size = 1024;
+    char buffer[buffer_size];
+    std::snprintf(buffer, buffer_size, fmt, std::forward<P1>(p1), std::forward<P>(params)...);
+    
+#ifdef _WIN32
+    HANDLE hConsole = (out == stdout) ? GetStdHandle(STD_OUTPUT_HANDLE) : GetStdHandle(STD_ERROR_HANDLE);
+    DWORD written;
+    WriteConsoleA(hConsole, buffer, strlen(buffer), &written, nullptr);
+    OutputDebugStringA(buffer);
+#else
+    if (out == stdout) {
+        std::cout << buffer;
+    } else {
+        std::cerr << buffer;
+    }
+#endif
 }
 
 } // namespace detail
